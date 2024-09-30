aa1e629cba08aa23bf00f1e2a2fea66556337a57
akshat246 <akshatverma2406@gmail.com>
2024-09-30 23:16:36 +0530

    final products page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,162 +1,158 @@
-import React, { useState } from 'react';
-import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
-import { RxDotFilled } from 'react-icons/rx';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y, EffectCoverflow, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/swiper-bundle.css';
+import { useState } from 'react';
 
 const Products = () => {
-  const slides = [
-    {
-      url: 'https://i.postimg.cc/HLG3QpSQ/b11.jpg',
-    },
-    {
-      url: 'https://i.postimg.cc/j5cX0yRX/b21.jpg',
-    },
-    {
-      url: 'https://i.postimg.cc/GmTq8BjL/b31.jpg',
-    },
+  
+  return ( <div>
+    <div className='homeproducts'>
+      <h1> Bags </h1>
+    </div>
+    <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y, EffectCoverflow, Autoplay]}
+        spaceBetween={10}
+        slidesPerView={2} // Default value for larger screens
+        breakpoints={{
+          // When the screen width is >= 760px, use this setting
+          760: {
+            slidesPerView: 3, // Or any number you want for larger screens
+          },
+          // When the screen width is <= 759px, use this setting
+          0: {
+            slidesPerView: 2, // Number of slides per view for smaller screens
+          },
+        }}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
 
-    {
-      url: 'https://i.postimg.cc/NfMNtC9r/DSC00658.jpg',
-    },
-    {
-      url: 'https://i.postimg.cc/Pxy25QG8/b41.jpg',
-    },
-    {
-      url: 'https://i.postimg.cc/Y0Sxpj9w/DSC00662.jpg',
-    },
-    {
-      url: 'https://i.postimg.cc/KjsD4Fk3/DSC00669.jpg',
-    },
-    {
-      url: 'https://i.postimg.cc/BvxppdyR/DSC00672.jpg',
-    },
+      >
+        <SwiperSlide><img className='homeproductspics' src={"https://i.postimg.cc/HLG3QpSQ/b11.jpg"} alt="bag" /></SwiperSlide>
+        <SwiperSlide><img className='homeproductspics' src={"https://i.postimg.cc/j5cX0yRX/b21.jpg"} alt="bag" /></SwiperSlide>
+        <SwiperSlide><img className='homeproductspics' src={"https://i.postimg.cc/GmTq8BjL/b31.jpg"} alt="bag" /></SwiperSlide>
+        <SwiperSlide><img className='homeproductspics' src={"https://i.postimg.cc/KjsD4Fk3/DSC00669.jpg"} alt="bag" /></SwiperSlide>
+        <SwiperSlide><img className='homeproductspics' src={"https://i.postimg.cc/qM1pMCYz/DSC00705.jpg"} alt="bag" /></SwiperSlide>
+        <SwiperSlide><img className='homeproductspics' src={"https://i.postimg.cc/4yBRdrWr/DSC00722.jpg"} alt="bag" /></SwiperSlide>
+        <SwiperSlide><img className='homeproductspics' src={"https://i.postimg.cc/505VcHL9/DSC00726.jpg"} alt="bag" /></SwiperSlide>
+        <SwiperSlide><img className='homeproductspics' src={"https://i.postimg.cc/T1RXTRJV/DSC00727.jpg"} alt="bag" /></SwiperSlide>
+        <SwiperSlide><img className='homeproductspics' src={"https://i.postimg.cc/NG4cpHQp/DSC00753.jpg"} alt="brown bag" /></SwiperSlide>
+        <SwiperSlide><img className='homeproductspics' src={"https://i.postimg.cc/FsLMdWTd/DSC00760.jpg"} alt="bag" /></SwiperSlide>
+        <SwiperSlide><img className='homeproductspics' src={"https://i.postimg.cc/JzC9bNFT/DSC00759.jpg"} alt="bag" /></SwiperSlide>
+        <SwiperSlide><img className='homeproductspics' src={"https://i.postimg.cc/FsLMdWTd/DSC00760.jpg"} alt="bag" /></SwiperSlide>
+        
 
-    {
-      url: 'https://i.postimg.cc/63t0mMxq/DSC00677.jpg',
-    },
-    
-  ];
+      </Swiper>
 
-  const slides1 = [
-    {
-      url: 'https://i.postimg.cc/sDpSwcQq/DSC00685.jpg',
-    },
-    {
-      url: 'https://i.postimg.cc/Xq9Z1jJd/DSC00686.jpg',
-    },
-    {
-      url: 'https://i.postimg.cc/3wwvrQSQ/DSC00688.jpg',
-    },
-
-    {
-      url: 'https://i.postimg.cc/fLGtvtwM/DSC00730.jpg',
-    },
-    {
-      url: 'https://i.postimg.cc/vZYJm0Wd/DSC00639.jpg',
-    },
-    {
-      url: 'https://i.postimg.cc/Zq0X8077/DSC00645.jpg',
-    },
-    {
-      url: 'https://i.postimg.cc/wMYPY26Z/DSC00646.jpg',
-    },
-    {
-      url: 'https://i.postimg.cc/rF1Tsd35/DSC00651.jpg',
-    },
-    {
-      url: 'https://i.postimg.cc/3w6Vn7zK/DSC00653.jpg',
-    },
-  ];
-
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const prevSlide = () => {
-    const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
-  };
-
-  const nextSlide = () => {
-    const isLastSlide = currentIndex === slides.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
-  };
-
-  const goToSlide = (slideIndex) => {
-    setCurrentIndex(slideIndex);
-  };
-
-  const [currentIndex1, setCurrentIndex1] = useState(0);
-
-  const prevSlide1 = () => {
-    const isFirstSlide1 = currentIndex1 === 0;
-    const newIndex1 = isFirstSlide1 ? slides1.length - 1 : currentIndex1 - 1;
-    setCurrentIndex1(newIndex1);
-  };
-
-  const nextSlide1 = () => {
-    const isLastSlide1 = currentIndex1 === slides1.length - 1;
-    const newIndex1 = isLastSlide1 ? 0 : currentIndex1 + 1;
-    setCurrentIndex1(newIndex1);
-  };
-
-  const goToSlide1 = (slide1Index) => {
-    setCurrentIndex1(slide1Index);
-  };
-
-  return (<div><div>Bags</div>
-    <div className='max-w-[1000px] h-[780px] w-full m-auto py-16 px-4 relative group'>
-      <div
-        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-        className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
-      ></div>
-      {/* Left Arrow */}
-      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-        <BsChevronCompactLeft onClick={prevSlide} size={30} />
-      </div>
-      {/* Right Arrow */}
-      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-        <BsChevronCompactRight onClick={nextSlide} size={30} />
-      </div>
-      <div className='flex top-4 justify-center py-2'>
-        {slides.map((slide, slideIndex) => (
-          <div
-            key={slideIndex}
-            onClick={() => goToSlide(slideIndex)}
-            className='text-2xl cursor-pointer'
-          >
-            <RxDotFilled />
-          </div>
-        ))}
-      </div>
+      <div className='homeproducts'>
+      <h1> Tiebacks </h1>
     </div>
 
-    <div>Pockets and Tiebacks</div>
-    <div className='max-w-[1000px] h-[780px] w-full m-auto py-16 px-4 relative group'>
-      <div
-        style={{ backgroundImage: `url(${slides1[currentIndex].url})` }}
-        className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
-      ></div>
-      {/* Left Arrow */}
-      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-        <BsChevronCompactLeft onClick={prevSlide1} size={30} />
+    <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y, EffectCoverflow, Autoplay]}
+        spaceBetween={10}
+        slidesPerView={2} // Default value for larger screens
+        breakpoints={{
+          // When the screen width is >= 760px, use this setting
+          760: {
+            slidesPerView: 3, // Or any number you want for larger screens
+          },
+          // When the screen width is <= 759px, use this setting
+          0: {
+            slidesPerView: 2, // Number of slides per view for smaller screens
+          },
+        }}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+
+      >
+        <SwiperSlide><img className='homeproductspics' src={"https://i.postimg.cc/zBF7Wh9g/DSC00646.jpg"} alt="Picture 1" /></SwiperSlide>
+        <SwiperSlide><img className='homeproductspics' src={"https://i.postimg.cc/kgLTdqQB/DSC00651.jpg"} alt="Picture 2" /></SwiperSlide>
+        <SwiperSlide><img className='homeproductspics' src={"https://i.postimg.cc/sD6nwp1P/DSC00708.jpg"} alt="Picture 3" /></SwiperSlide>
+        <SwiperSlide><img className='homeproductspics' src={"https://i.postimg.cc/FFj5WnQy/DSC00820.jpg"} alt="Picture 4" /></SwiperSlide>
+
+      </Swiper>
+
+      <div className='homeproducts'>
+        <h1> Wall Pockets</h1>
       </div>
-      {/* Right Arrow */}
-      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-        <BsChevronCompactRight onClick={nextSlide1} size={30} />
-      </div>
-      <div className='flex top-4 justify-center py-2'>
-        {slides1.map((slide, slideIndex) => (
-          <div
-            key={slideIndex}
-            onClick={() => goToSlide(slideIndex)}
-            className='text-2xl cursor-pointer'
-          >
-            <RxDotFilled />
-          </div>
-        ))}
-      </div>
-    </div>
-    </div>
+
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y, EffectCoverflow, Autoplay]}
+        spaceBetween={10}
+        slidesPerView={2} // Default value for larger screens
+        breakpoints={{
+          // When the screen width is >= 760px, use this setting
+          760: {
+            slidesPerView: 3, // Or any number you want for larger screens
+          },
+          // When the screen width is <= 759px, use this setting
+          0: {
+            slidesPerView: 2, // Number of slides per view for smaller screens
+          },
+        }}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+
+      >
+
+        <SwiperSlide><img className='homeproductspics' src={"https://i.postimg.cc/3wwvrQSQ/DSC00688.jpg"} alt="Picture 6" /></SwiperSlide>
+
+      </Swiper>
+
+      
+  </div>
   );
 }
 
